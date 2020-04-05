52b705b8e1f02c7435936b06fc6dcec812a54f3f
Cheikhany ould Ejiwen <skibnewrim@gmail.com>
2020-04-04 21:55:53 -0700

    add some data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
 import React from "react";
 import "./styles.css";
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'JS',
+    url: 'https://js.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 1,
+  },
+  {
+    title: 'css',
+    url: 'https://css.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 2,
+  },
+];
+
+
 export default function App() {
   return (
     <div className="App">
